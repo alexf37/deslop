57e267a72ea3b9babcbd86564f24cfecc77cd77e
Alex Foster <xrk4np@virginia.edu>
2024-05-22 00:54:07 -0400

    ditch plasmo, back to plain js

--- background.ts
+++
@@ -1,9 +0,0 @@
-chrome.webNavigation.onCommitted.addListener(
-  function (details) {
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      files: ["content.js"]
-    })
-  },
-  { url: [{ hostSuffix: "google.com", pathPrefix: "/search" }] }
-)
